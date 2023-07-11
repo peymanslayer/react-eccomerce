5d2a2b7f43d6cb02e5ee908de312393cd25f6917
peymanslayer <peymantaghitash2024@gmail.com>
2023-07-11 22:47:16 +0430

    add productSellerWarranty component

--- eccomerce-app/src/components/product/productSeller.tsx
+++ eccomerce-app/src/components/product/productSeller.tsx
@@ -1,13 +1,11 @@
+import ProductSellerWarranty from "./productSellerWarranty";
 import ShopOfProductSeller from "./shopOfProductSeller";
 
 function ProductSeller(){
     return(
         <div className="w-72 h-96 border rounded bg-slate-100 flex flex-col justify-start items-around">
             <ShopOfProductSeller/>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
+            <ProductSellerWarranty/>
         </div>
     )
 }
--- eccomerce-app/src/components/product/productSellerWarranty.tsx
+++ eccomerce-app/src/components/product/productSellerWarranty.tsx
@@ -1,5 +1,15 @@
-function ProductSellerWarranty(){
-    return(
-        
-    )
+import { AiOutlineSafetyCertificate } from "react-icons/ai";
+
+function ProductSellerWarranty() {
+  return (
+    <>
+      <div className="w-3/4 h-10 flex flex-row justify-around items-center">
+        <AiOutlineSafetyCertificate size={30} />
+        <p className="font-bold text-sm">همراه با گارانتی اصل</p>
+      </div>
+      <hr className="w-72 h-1 bg-slate-300" />
+    </>
+  );
 }
+
+export default ProductSellerWarranty;
