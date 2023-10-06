8b1e8f886ffafbaa45543cf212af57b572c240e3
peymanslayer <peymantaghitash2024@gmail.com>
2023-10-06 21:28:01 +0330

    add constant folder and constant reducerPath

---
+++ eccomerce-app/src/constant/apiConstants.ts
@@ -0,0 +1,4 @@
+export const findProductConst='findProductApi';
+export const amazingOfferListConst='amazingOfferListApi';
+export const realtedProductConst='realtedProductApi';
+export const topSliderConst='topSliderReducer';
